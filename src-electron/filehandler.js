60eb386b455fa80b48903882c0ddc92cb7b39a7e
Tim K <tkaelbel@web.de>
2022-02-05 16:14:56 +0100

    added logo. improvement of startup time

--- src-electron/filehandler.js
+++ src-electron/filehandler.js
@@ -1,12 +1,12 @@
 import { ipcMain } from 'electron';
-import { writeFileSync, readFileSync } from 'fs';
+import fs from 'fs';
 
 export function useFileHandler() {
-  ipcMain.handle('fileHandler:readFile', async (event, path) => {
-    return readFileSync(path, 'utf8');
+  ipcMain.handle('fileHandler:readFile', (event, path) => {
+    return fs.promises.readFile(path, 'utf8');
   });
 
-  ipcMain.handle('fileHandler:writeFile', async (event, path, data) => {
-    return writeFileSync(path, data);
+  ipcMain.handle('fileHandler:writeFile', (event, path, data) => {
+    return fs.promises.writeFile(path, data);
   });
 }
